c33250e17117d719db02658b49f800d94a134ee9
Taras Bilinskyi <tarasbilinskyi@Mac-mini-Taras.local>
2017-01-26 03:42:20 +0200

    v0.3

--- app/js/main.js
+++ app/js/main.js
@@ -1,54 +1,132 @@
 
+
+
+
+
+
 $(document).ready(function(){
 
-  $(document).on('click', '.catalog-wrapp-item:not(.add)', function(e) {
-    e.preventDefault(); 
-
-    if ($('.catalog-wrapp-item.add').length) {
-      $('.catalog-wrapp-item.add').after($('.item-c + .hide-content'));
-      $('.catalog-wrapp-item').removeClass('add');
-    }
-
-    var thisEl = $(this).parent();
-    var thisElPos = thisEl.position().top;
-
-    if ($(this).parent().next().length) {
-      while ( (thisEl.next().length) && (thisElPos == thisEl.next().position().top) ) {
-        thisEl= thisEl.next();  
-      }
-    }
-
-    var lastElement = thisEl;
-    console.log(lastElement.index());
-    $(this).addClass('add');
-    var content = $(this).next();
-    lastElement.after(content);
-  });
 
 
 
-  $('.catalog-wrapp-item').on('click', '.rate > *, .size-m, .calc-res *', function(e) {
-    e.stopPropagation();
-  });
 
 
-  $(document).on('click', '.icon-close-content', function(e) {
-    e.preventDefault(); 
-    $('.catalog-wrapp-item.add').after($('.item-c + .hide-content'));
-    $('.catalog-wrapp-item').removeClass('add');
 
-  });
 
-  $('.select-1').ikSelect({
-    autoWidth: false,
-    ddFullWidth: false
-  });
+	$(document).on('click', '.catalog-wrapp-item:not(.add)', function(e) {
+		e.preventDefault(); 
 
-  $('.mh-1').matchHeight();
-  $('.catalog-wrapp-item').matchHeight();
-  $('.wrapp-p').matchHeight({
-   byRow: false 
- })
+		if ($('.catalog-wrapp-item.add').length) {
+			$('.catalog-wrapp-item.add').after($('.item-c + .hide-content'));
+			$('.catalog-wrapp-item').removeClass('add');
+		}
+
+		var thisEl = $(this).parent();
+		var thisElPos = thisEl.position().top;
+
+		if ($(this).parent().next().length) {
+			while ( (thisEl.next().length) && (thisElPos == thisEl.next().position().top) ) {
+				thisEl= thisEl.next();  
+			}
+		}
+
+		var lastElement = thisEl;
+		console.log(lastElement.index());
+		$(this).addClass('add');
+		var content = $(this).next();
+		lastElement.after(content);
+	});
+
+
+
+	$('.catalog-wrapp-item').on('click', '.rate > *, .size-m, .calc-res *', function(e) {
+		e.stopPropagation();
+	});
+
+
+	$(document).on('click', '.icon-close-content', function(e) {
+		e.preventDefault(); 
+		$('.catalog-wrapp-item.add').after($('.item-c + .hide-content'));
+		$('.catalog-wrapp-item').removeClass('add');
+
+	});
+
+	$('.select-1').ikSelect({
+		autoWidth: false,
+		ddFullWidth: false
+	});
+
+	$('.mh-1').matchHeight();
+	$('.catalog-wrapp-item').matchHeight();
+	$('.wrapp-p').matchHeight({
+		byRow: false 
+	})
+
+
+
+
+
+
+
+
+	if($('.carousel-opinions').length) {
+		$('.carousel-opinions').slick({
+			dots: true,
+			infinite: true,
+			speed: 500,
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			appendArrows: $(".carousel-opinions--arrows"),
+			appendDots: $(".carousel-opinions--dots"),
+			responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+			]
+		});
+	}
+
+
+	if($('.carousel-man').length) {
+		$('.carousel-man').slick({
+			dots: false,
+			infinite: true,
+			speed: 500,
+			slidesToShow: 6,
+			slidesToScroll: 1,
+			responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 4
+				}
+			},
+			{
+				breakpoint: 767,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 479,
+				settings: {
+					slidesToShow: 2
+				}
+			}
+			]
+		});
+	}
+
+
 // $('#slider-1').bxSlider({
 // 		controls: true, 
 // 		pager: false,
@@ -92,10 +170,40 @@ $(document).ready(function(){
 // });
 
 $('input,textarea').focus(function(){
- $(this).data('placeholder',$(this).attr('placeholder'))
- .attr('placeholder','');
+	$(this).data('placeholder',$(this).attr('placeholder'))
+	.attr('placeholder','');
 }).blur(function(){
- $(this).attr('placeholder',$(this).data('placeholder'));
+	$(this).attr('placeholder',$(this).data('placeholder'));
 });
 
 });
+
+
+
+
+
+
+
+
+$(window).on('load', function() {
+
+	footerHeight();
+
+});
+
+$(window).resize(function(){
+	footerHeight();
+})
+
+
+
+
+
+// count footer height to pos bottom 
+function footerHeight () {
+	var footerHeight = $('.footer').outerHeight(),
+	bodyHeight = $('.wrapper-p').outerHeight();
+
+	$('.wrapper-p').css('paddingBottom', footerHeight);
+
+};
